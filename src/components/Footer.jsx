80062007de815301d7c0c941144742fddcc14c48
Mohd Talish Ansari <92112059+Talish1234@users.noreply.github.com>
2025-07-24 17:41:37 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className={styles.footer}>
             <p>&copy; {new Date().getFullYear()} TravelExplorer. All rights reserved.</p>
             <div className={styles.socialLinks}>
-                <a href="https://www.linkedin.com/in/mohd-talish-ansari-a8a05b234/" target="_blank">Linkend</a>
+                <a href="https://www.linkedin.com/in/mohd-talish-ansari-a8a05b234/" target="_blank">Linkedin</a>
                 <a href="https://leetcode.com/u/Talish1234/" target="_blank">LeetCode</a>
                 <a href="https://github.com/Talish1234/TravelExplorer.git" target="_blank">Github</a>
             </div>
